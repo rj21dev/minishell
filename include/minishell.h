60f9dfdc0e00cf94c8f91c1fe913206c94bb6c06
Rennifer Jada <rjada@student.21-school.ru>
2022-05-22 16:32:39 +0300

    Feat: add readline, handle history, handle signals

--- include/minishell.h
+++ include/minishell.h
@@ -2,5 +2,11 @@
 # define MINISHELL_H
 
 # include "../libft/libft.h"
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <signal.h>
+
+#define CLOSE "\001\033[0m\002"
+#define BEGIN(x,y) "\001\033["#x";"#y"m\002"
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,5 +1,14 @@
 #include "../include/minishell.h"
 
+static void	sig_handler(int sig)
+{
+	if (sig == SIGINT)
+		ft_putendl_fd("", STDOUT);
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
+}
+
 int	main(int argc, char **argv, char **envp)
 {
 	(void)argv;
@@ -8,6 +17,16 @@ int	main(int argc, char **argv, char **envp)
 	if (argc != 1)
 		return (1);
 
-	ft_putendl_fd("fuckingshell is under cunstruction!", STDOUT);
+	signal(SIGINT, sig_handler);
+	signal(SIGQUIT, SIG_IGN);
+	while(1)
+	{
+		char *str = readline(BEGIN(49, 34)"fuckingshell $ "CLOSE);
+		if (!str)
+			break;
+		add_history(str);
+		free(str);
+	}
+	ft_putendl_fd("exit", STDOUT);
 	return (0);
 }
